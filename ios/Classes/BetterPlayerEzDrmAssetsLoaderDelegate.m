40ca4bd0fe5b31ce2359659870caa239352644c7
Khaled <spkhalad@gmail.com>
2025-05-07 13:49:58 +0600

    fix: Weird shenanigans on FairPlay DRM

--- ios/Classes/BetterPlayerEzDrmAssetsLoaderDelegate.m
+++ ios/Classes/BetterPlayerEzDrmAssetsLoaderDelegate.m
@@ -8,9 +8,9 @@
 
 NSString *_assetId;
 
-NSString * DEFAULT_LICENSE_SERVER_URL = @"https://fps.ezdrm.com/api/licenses/";
+NSString *DEFAULT_LICENSE_SERVER_URL = @"https://fps.ezdrm.com/api/licenses/";
 
-- (instancetype)init:(NSURL *)certificateURL withLicenseURL:(NSURL *)licenseURL{
+- (instancetype)init:(NSURL *)certificateURL withLicenseURL:(NSURL *)licenseURL {
     self = [super init];
     _certificateURL = certificateURL;
     _licenseURL = licenseURL;
@@ -24,27 +24,27 @@ NSString * DEFAULT_LICENSE_SERVER_URL = @"https://fps.ezdrm.com/api/licenses/";
  ** Takes the bundled SPC and sends it to the license server defined at licenseUrl or KEY_SERVER_URL (if licenseUrl is null).
  ** It returns CKC.
  ** ---------------------------------------*/
-- (NSData *)getContentKeyAndLeaseExpiryFromKeyServerModuleWithRequest:(NSData*)requestBytes and:(NSString *)assetId and:(NSString *)customParams and:(NSError *)errorOut {
-    NSData * decodedData;
-    NSURLResponse * response;
-    
-    NSURL * finalLicenseURL;
-    if (_licenseURL != [NSNull null]){
+- (NSData *)getContentKeyAndLeaseExpiryFromKeyServerModuleWithRequest:(NSData *)requestBytes and:(NSString *)assetId and:(NSString *)customParams and:(NSError *)errorOut {
+    NSData *decodedData;
+    NSURLResponse *response;
+
+    NSURL *finalLicenseURL;
+    if (_licenseURL != [NSNull null]) {
         finalLicenseURL = _licenseURL;
     } else {
-        finalLicenseURL = [[NSURL alloc] initWithString: DEFAULT_LICENSE_SERVER_URL];
+        finalLicenseURL = [[NSURL alloc] initWithString:DEFAULT_LICENSE_SERVER_URL];
     }
-    NSURL * ksmURL = [[NSURL alloc] initWithString: [NSString stringWithFormat:@"%@%@%@",finalLicenseURL,assetId,customParams]];
+    // NSURL * ksmURL = [[NSURL alloc] initWithString: [NSString stringWithFormat:@"%@%@%@",finalLicenseURL,assetId,customParams]];
     
-    NSMutableURLRequest * request = [[NSMutableURLRequest alloc] initWithURL:ksmURL];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:finalLicenseURL];
     [request setHTTPMethod:@"POST"];
     [request setValue:@"application/octet-stream" forHTTPHeaderField:@"Content-type"];
     [request setHTTPBody:requestBytes];
-    
+
     @try {
         decodedData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
     }
-    @catch (NSException* excp) {
+    @catch (NSException *excp) {
         NSLog(@"SDK Error, SDK responded with Error: (error)");
     }
     return decodedData;
@@ -58,51 +58,52 @@ NSString * DEFAULT_LICENSE_SERVER_URL = @"https://fps.ezdrm.com/api/licenses/";
  ** the example here uses a local certificate
  ** but you may need to edit this function to point to your certificate
  ** ---------------------------------------*/
-- (NSData *)getAppCertificate:(NSString *) String {
-    NSData * certificate = nil;
+- (NSData *)getAppCertificate:(NSString *)String {
+    NSData *certificate = nil;
     certificate = [NSData dataWithContentsOfURL:_certificateURL];
     return certificate;
 }
 
 - (BOOL)resourceLoader:(AVAssetResourceLoader *)resourceLoader shouldWaitForLoadingOfRequestedResource:(AVAssetResourceLoadingRequest *)loadingRequest {
     NSURL *assetURI = loadingRequest.request.URL;
-    NSString * str = assetURI.absoluteString;
-    NSString * mySubstring = [str substringFromIndex:str.length - 36];
+    NSString *str = assetURI.absoluteString;
+    NSString *mySubstring = [str substringFromIndex:str.length - 36];
     _assetId = mySubstring;
-    NSString * scheme = assetURI.scheme;
-    NSData * requestBytes;
-    NSData * certificate;
-    if (!([scheme isEqualToString: @"skd"])){
+    NSString *scheme = assetURI.scheme;
+    NSData *requestBytes;
+    NSData *certificate;
+    if (!([scheme isEqualToString:@"skd"])) {
         return NO;
     }
     @try {
         certificate = [self getAppCertificate:_assetId];
     }
-    @catch (NSException* excp) {
+    @catch (NSException *excp) {
         [loadingRequest finishLoadingWithError:[[NSError alloc] initWithDomain:NSURLErrorDomain code:NSURLErrorClientCertificateRejected userInfo:nil]];
     }
     @try {
-        requestBytes = [loadingRequest streamingContentKeyRequestDataForApp:certificate contentIdentifier: [str dataUsingEncoding:NSUTF8StringEncoding] options:nil error:nil];
+        requestBytes = [loadingRequest streamingContentKeyRequestDataForApp:certificate contentIdentifier:[str dataUsingEncoding:NSUTF8StringEncoding] options:nil error:nil];
     }
-    @catch (NSException* excp) {
+    @catch (NSException *excp) {
         [loadingRequest finishLoadingWithError:nil];
         return YES;
     }
-    
-    NSString * passthruParams = [NSString stringWithFormat:@"?customdata=%@", _assetId];
-    NSData * responseData;
-    NSError * error;
-    
+
+    NSString *passthruParams = [NSString stringWithFormat:@"?customdata=%@", _assetId];
+    NSData *responseData;
+    NSError *error;
+
     responseData = [self getContentKeyAndLeaseExpiryFromKeyServerModuleWithRequest:requestBytes and:_assetId and:passthruParams and:error];
-    
-    if (responseData != nil && responseData != NULL && ![responseData.class isKindOfClass:NSNull.class]){
-        AVAssetResourceLoadingDataRequest * dataRequest = loadingRequest.dataRequest;
+
+    if (responseData != nil && responseData != NULL &&
+        ![responseData.class isKindOfClass:NSNull.class]) {
+        AVAssetResourceLoadingDataRequest *dataRequest = loadingRequest.dataRequest;
         [dataRequest respondWithData:responseData];
         [loadingRequest finishLoading];
     } else {
         [loadingRequest finishLoadingWithError:error];
     }
-    
+
     return YES;
 }
 
